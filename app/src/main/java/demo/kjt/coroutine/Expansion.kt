6c24b0b245d0e48d29f33c40dd99f006fc3d868a
kjt <jiangtao.kang@lanmaoly.com>
2019-07-31 18:09:20 +0800

    change simple

--- app/src/main/java/demo/kjt/coroutine/Expansion.kt
+++ app/src/main/java/demo/kjt/coroutine/Expansion.kt
@@ -7,30 +7,35 @@ import com.bumptech.glide.load.resource.drawable.DrawableTransitionOptions
 import com.bumptech.glide.request.RequestOptions
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.GlobalScope
-import kotlinx.coroutines.channels.Channel
 import kotlinx.coroutines.channels.actor
-import kotlinx.coroutines.launch
 
 /**
  * Created by kjt on 2019-07-30
  */
 fun View.onClick(action: suspend (View) -> Unit) {
+    var currentTime: Long
+    var lastTime = 0L
     val eventAction = GlobalScope.actor<View>(Dispatchers.Main) {
-        for (event in channel) action(event)
+            for (event in channel) action(event)
     }
     setOnClickListener {
-        eventAction.offer(it)
+        currentTime = System.currentTimeMillis()
+        if (currentTime - lastTime >= 1500) {
+            lastTime = currentTime
+            eventAction.offer(it)
+        }
     }
-    val channel = Channel<View>(capacity = Channel.RENDEZVOUS)
+    /*val channel = Channel<View>(capacity = Channel.RENDEZVOUS)
     GlobalScope.launch(Dispatchers.Main) {
         for (item in channel) action(item)
     }
     setOnClickListener {
         GlobalScope.launch { channel.offer(it) }
-    }
+    }*/
 }
 
 fun ImageView.loadUrl(url: String) {
     val options = RequestOptions.fitCenterTransform().placeholder(R.mipmap.ic_image_loading)
-    Glide.with(this.context).setDefaultRequestOptions(options).load(url).transition(DrawableTransitionOptions.withCrossFade()).into(this)
+    Glide.with(this.context).setDefaultRequestOptions(options).load(url)
+        .transition(DrawableTransitionOptions.withCrossFade()).into(this)
 }
--- app/src/main/java/demo/kjt/coroutine/activity/MainActivity.kt
+++ app/src/main/java/demo/kjt/coroutine/activity/MainActivity.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import demo.kjt.coroutine.R
+import demo.kjt.coroutine.onClick
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -11,13 +12,13 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        btn_countdown.setOnClickListener {
+        btn_countdown.onClick {
             startActivity(Intent(this, CountDownActivity::class.java))
         }
-        btn_fib.setOnClickListener {
+        btn_fib.onClick {
             startActivity(Intent(this, FibActivity::class.java))
         }
-        btn_news.setOnClickListener {
+        btn_news.onClick {
             startActivity(Intent(this, NewsActivity::class.java))
         }
     }
